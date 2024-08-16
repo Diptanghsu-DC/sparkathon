c799977c0b94c500f9540760c2db02266d5d2d1c
Diptanghsu-DC <115690935+Diptanghsu-DC@users.noreply.github.com>
2024-08-16 22:28:30 +0530

    ar and 3d integrated

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:sparkathon_project/ar_feature/ar_feature.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,29 +13,35 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width*0.06),
+        body: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Container(
+              padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.06),
               alignment: Alignment.topLeft,
-              child: Text("Catagories", style: Theme.of(context).textTheme.headlineMedium,)
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height*0.025,),
-            CarouselSlider(
+              child: Text(
+                "Catagories",
+                style: Theme.of(context).textTheme.headlineMedium,
+              )),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.025,
+          ),
+          CarouselSlider(
               items: [
                 InkWell(
                   child: Card(
                     color: Colors.indigoAccent,
                     elevation: 30,
                     child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      height: 350,
-                      child: Image.asset("assets/image/clothing.jpg",)
-                    ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        height: 350,
+                        child: Image.asset(
+                          "assets/image/clothing.jpg",
+                        )),
                   ),
                 ),
                 InkWell(
@@ -42,11 +49,11 @@ class _HomeScreenState extends State<HomeScreen> {
                     elevation: 30,
                     color: Colors.indigoAccent,
                     child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      height: 350,
-                      child: Image.asset("assets/image/furniture.jpeg")),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        height: 350,
+                        child: Image.asset("assets/image/furniture.jpeg")),
                   ),
                 ),
                 InkWell(
@@ -54,11 +61,13 @@ class _HomeScreenState extends State<HomeScreen> {
                     color: Colors.indigoAccent,
                     elevation: 30,
                     child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      height: 350,
-                      child: Image.asset("assets/image/appliances.jpeg",)),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        height: 350,
+                        child: Image.asset(
+                          "assets/image/appliances.jpeg",
+                        )),
                   ),
                 ),
                 InkWell(
@@ -66,63 +75,74 @@ class _HomeScreenState extends State<HomeScreen> {
                     color: Colors.indigoAccent,
                     elevation: 30,
                     child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      height: 350,
-                      child: Image.asset("assets/image/gadgets.jpeg", fit: BoxFit.cover,)
-                      
-                      ),
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        height: 350,
+                        child: Image.asset(
+                          "assets/image/gadgets.jpeg",
+                          fit: BoxFit.cover,
+                        )),
                   ),
+                  onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ARFeatureScreen())),
                 ),
-              ], 
+              ],
               options: CarouselOptions(
                 // enlargeCenterPage: true,
                 viewportFraction: 1,
                 autoPlay: true,
-                clipBehavior : Clip.antiAlias,
-              ) 
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height*0.025,),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width*0.06),
+                clipBehavior: Clip.antiAlias,
+              )),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.025,
+          ),
+          Container(
+              padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.06),
               alignment: Alignment.topLeft,
-              child: Text("Hot Deals", style: Theme.of(context).textTheme.headlineMedium,)
-            ),
-            Expanded(
+              child: Text(
+                "Hot Deals",
+                style: Theme.of(context).textTheme.headlineMedium,
+              )),
+          Expanded(
               child: Container(
-              margin: EdgeInsets.all(20),
-              child: GridView(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10
-                  ),
-                  children: [
-                    Card(
-                      elevation: 20,
-                      child: Container(
-                        height: MediaQuery.of(context).size.height*0.2,
-                        width: MediaQuery.of(context).size.width*0.4,
-                        child: Image.asset("assets/image/watches_discount.png", fit: BoxFit.cover,),
+                  margin: EdgeInsets.all(20),
+                  child: GridView(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10),
+                    children: [
+                      Card(
+                        elevation: 20,
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.2,
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          child: Image.asset(
+                            "assets/image/watches_discount.png",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                    ),
-                    Card(
-                      elevation: 20,
-                      child: Container(
-                        height: MediaQuery.of(context).size.height*0.2,
-                        width: MediaQuery.of(context).size.width*0.4,
-                        child: Image.asset("assets/image/watches_discount.png", fit: BoxFit.cover,),
+                      Card(
+                        elevation: 20,
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.2,
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          child: Image.asset(
+                            "assets/image/watches_discount.png",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                    ),
-                  ],
-                ) 
-              )
-            )
-          ],
-        ),
-      )
-    );
+                    ],
+                  )))
+        ],
+      ),
+    ));
   }
 }
